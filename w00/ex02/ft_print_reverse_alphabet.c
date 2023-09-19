0f2dd0508255a79bff8b64997662305d41eadb61
tiger wong <wongappcontact@gmail.com>
2023-09-19 10:13:48 +0800

    for loop reverse

--- w00/ex02/ft_print_reverse_alphabet.c
+++ w00/ex02/ft_print_reverse_alphabet.c
@@ -1,32 +1,46 @@
 #include <unistd.h>
 
-void ft_print_reverse_alphabet(void){
-    char display_char;
-    display_char = 'z'; write(1,&display_char,1);
-		display_char = 'y'; write(1,&display_char,1);
-		display_char = 'x'; write(1,&display_char,1);
-		display_char = 'w'; write(1,&display_char,1);
-		display_char = 'v'; write(1,&display_char,1);
-		display_char = 'u'; write(1,&display_char,1);
-		display_char = 't'; write(1,&display_char,1);
-		display_char = 's'; write(1,&display_char,1);
-		display_char = 'r'; write(1,&display_char,1);
-		display_char = 'q'; write(1,&display_char,1);
-		display_char = 'p'; write(1,&display_char,1);
-		display_char = 'o'; write(1,&display_char,1);
-		display_char = 'n'; write(1,&display_char,1);
-		display_char = 'm'; write(1,&display_char,1);
-		display_char = 'l'; write(1,&display_char,1);
-		display_char = 'k'; write(1,&display_char,1);
-		display_char = 'j'; write(1,&display_char,1);
-		display_char = 'i'; write(1,&display_char,1);
-		display_char = 'h'; write(1,&display_char,1);
-		display_char = 'g'; write(1,&display_char,1);
-		display_char = 'f'; write(1,&display_char,1);
-		display_char = 'e'; write(1,&display_char,1);
-		display_char = 'd'; write(1,&display_char,1);
-		display_char = 'c'; write(1,&display_char,1);
-		display_char = 'b'; write(1,&display_char,1);
-		display_char = 'a'; write(1,&display_char,1);
-		display_char = '\n'; write(1,&display_char,1);
+void ft_putchar(char c) {
+    write(1, &c, 1);
 }
+
+void ft_print_reverse_alphabet(void) {
+    char letter;
+
+    for (letter = 'z'; letter >= 'a'; letter--) {
+        ft_putchar(letter);
+    }
+
+    ft_putchar('\n');
+}
+
+// void ft_print_reverse_alphabet(void){
+//     char display_char;
+//     display_char = 'z'; write(1,&display_char,1);
+// 		display_char = 'y'; write(1,&display_char,1);
+// 		display_char = 'x'; write(1,&display_char,1);
+// 		display_char = 'w'; write(1,&display_char,1);
+// 		display_char = 'v'; write(1,&display_char,1);
+// 		display_char = 'u'; write(1,&display_char,1);
+// 		display_char = 't'; write(1,&display_char,1);
+// 		display_char = 's'; write(1,&display_char,1);
+// 		display_char = 'r'; write(1,&display_char,1);
+// 		display_char = 'q'; write(1,&display_char,1);
+// 		display_char = 'p'; write(1,&display_char,1);
+// 		display_char = 'o'; write(1,&display_char,1);
+// 		display_char = 'n'; write(1,&display_char,1);
+// 		display_char = 'm'; write(1,&display_char,1);
+// 		display_char = 'l'; write(1,&display_char,1);
+// 		display_char = 'k'; write(1,&display_char,1);
+// 		display_char = 'j'; write(1,&display_char,1);
+// 		display_char = 'i'; write(1,&display_char,1);
+// 		display_char = 'h'; write(1,&display_char,1);
+// 		display_char = 'g'; write(1,&display_char,1);
+// 		display_char = 'f'; write(1,&display_char,1);
+// 		display_char = 'e'; write(1,&display_char,1);
+// 		display_char = 'd'; write(1,&display_char,1);
+// 		display_char = 'c'; write(1,&display_char,1);
+// 		display_char = 'b'; write(1,&display_char,1);
+// 		display_char = 'a'; write(1,&display_char,1);
+// 		display_char = '\n'; write(1,&display_char,1);
+// }
